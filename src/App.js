20524ff0b49bc8682176eeb434e5c65e80a597fc
Clovis Rosa <35171486+clovis-rosa@users.noreply.github.com>
2020-05-19 15:57:40 +0100

    Added routes, navigation and correct viewport switch update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,80 @@
-import React, { useEffect } from 'react'
-import gsap from 'gsap';
+import React, { useEffect, useState } from 'react'
+import { Route } from 'react-router-dom'
+import gsap from 'gsap'
 import './styles/App.scss'
-import Header from './components/Header'
-import Home from './pages/Home';
 
+// Components
+import Header from './components/Header'
+import Navegation from './components/Navigation'
+
+// Pages
+import Home from './pages/Home'
+import CaseStudies from './pages/CaseStudies'
+import Approach from './pages/Aprroach'
+import Services from './pages/Services'
+import About from './pages/About'
 
 // Routers
+const routes = [
+  { path: '/', name: 'Home', Components: Home },
+  { path: '/case-studies', name: 'Case Studies', Components: CaseStudies },
+  { path: '/aprroach', name: 'Aprroach', Components: Approach },
+  { path: '/Services', name: 'Services', Components: Services },
+  { path: '/about-us', name: 'About Us', Components: About },
+]
+
+// Set 1s delay for the check dimensions
+function debounce(fn, ms) {
+  let timer
+  clearTimeout(timer)
+  timer = setTimeout(() => {
+    timer = null
+    fn.apply(this, arguments)
+  }, ms)
+}
 
 function App() {
+  // Correct viewport swich screen due javascript not updating te viewport height
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  })
 
   useEffect(() => {
-    // Calc the vh 50%
-    let vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-    // Prevent white text to flashing
+    // Preventing flash from happening
     gsap.to('body', 0, { css: { visibility: 'visible' } })
 
+    // Grab inner hitght of window for mobile reasons when dealing vh
+    let vh = dimensions.height * 0.01
+    // Set CSS variable vh
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
 
+    const debouncedHandleResize = debounce(function HandleResize() {
+      // Set the state of height and width
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      })
+    }, 1000)
+
+    window.addEventListener('resize', debouncedHandleResize)
+
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize)
+    }
   }, [])
 
   return (
     <>
-      <Header />
-      <Home />
+      <Header dimensions={dimensions} />
+      <div className="App">
+        {routes.map(({ path, Components }) => (
+          <Route key={path} exact path={path}>
+            <Components dimensions={dimensions} />
+          </Route>
+        ))}
+      </div>
+      <Navegation />
     </>
   )
 }
